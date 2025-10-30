d4e91449335d365c67210a9783ec14cec871df16
raviraj879214 <ravirajalphainfo@gmail.com>
2025-10-30 16:58:59 +0530

    Upload side bar

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -125,7 +125,7 @@ const navItems: NavItem[] = [
             { name: "Roles", path: "/admin/manage-roles", pro: false },
             { name: "Assign Modules", path: "/admin/assign-modules", pro: false },
             { name: "Manage User", path: "/admin/manage-user", pro: false },
-            { name: "Test Modules", path: "/admin/test", pro: false },
+            // { name: "Test Modules", path: "/admin/test", pro: false },
           ],
         },
       ]
@@ -154,17 +154,17 @@ const navItems: NavItem[] = [
         path: "/admin/manage-seo-content",
     },
 
-     {
-        icon: <EnvelopeIcon />,
-        name: "Manage Specialties",
-        path: "/admin/manage-treatment",
-    },
+    //  {
+    //     icon: <EnvelopeIcon />,
+    //     name: "Manage Specialties",
+    //     path: "/admin/manage-treatment",
+    // },
 
-     {
-        icon: <EnvelopeIcon />,
-        name: "Manage Category",
-        path: "/admin/manage-category",
-    },
+    //  {
+    //     icon: <EnvelopeIcon />,
+    //     name: "Manage Category",
+    //     path: "/admin/manage-category",
+    // },
     
     
 
