44e3bf98ed95ba0bc97f54ecbacb6b9e542bd15d
raviraj879214 <ravirajalphainfo@gmail.com>
2025-10-30 11:44:53 +0530

    test

--- src/components/user-profile/UserInfoCard.tsx
+++ src/components/user-profile/UserInfoCard.tsx
@@ -37,6 +37,7 @@ export default function UserInfoCard({ user, sendUpdatedata }: UserMetaCardProps
       phone: user.phone,
       Bio: user.Bio,
     },
+    
   });
 
   const onUpdate = (data: UpdateData) => {
