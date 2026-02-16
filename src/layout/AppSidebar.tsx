7e5a3bdf8846ea553c30cb33faefbd0920ff18f8
raviraj879214 <ravirajalphainfo@gmail.com>
2026-02-16 11:29:37 +0530

    adin sidebar

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -151,17 +151,17 @@ const navItems: NavItem[] = [
 
   
 
-    {
-        icon: (
-          <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2"
-        strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
-        <rect x="3" y="5" width="18" height="14" rx="2" />
-        <path d="M3 7l9 6 9-6" />
-      </svg>
-        ),
-        name: "Manage Email Templates",
-        path: "/admin/manage-email-templates",
-    },
+    // {
+    //     icon: (
+    //       <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2"
+    //     strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+    //     <rect x="3" y="5" width="18" height="14" rx="2" />
+    //     <path d="M3 7l9 6 9-6" />
+    //   </svg>
+    //     ),
+    //     name: "Manage Email Templates",
+    //     path: "/admin/manage-email-templates",
+    // },
      {
          icon: (
       <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2"
