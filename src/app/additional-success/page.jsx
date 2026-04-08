3eaea07b4bdb8b90c4423f11f7fb175e4508a8e2
raviraj879214 <ravirajalphainfo@gmail.com>
2026-04-08 17:49:43 +0530

    some minor changes

--- src/app/additional-success/page.jsx
+++ src/app/additional-success/page.jsx
@@ -1,17 +1,21 @@
+// app/additional-success/page.js
+import { Suspense } from "react";
 import { Success } from "../../components/(AdditionalServices)/(additionalsuccess)/succes";
 
 export const metadata = {
-  title: "Success | "  + `${process.env.NEXT_PUBLIC_PROJECT_NAME}`,
+  title: "Success | " + `${process.env.NEXT_PUBLIC_PROJECT_NAME}`,
 };
 
-
-
 export default function Page() {
-    
-    return (
-        <>
-         
-            <Success />
-        </>
-    );
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-screen">
+          <p className="text-gray-600 text-lg animate-pulse">Loading...</p>
+        </div>
+      }
+    >
+      <Success />
+    </Suspense>
+  );
 }
