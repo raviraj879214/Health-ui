67fa2a3e0f535707667129400bc7f5a0b63c9d01
raviraj879214 <ravirajalphainfo@gmail.com>
2026-06-13 13:30:12 +0530

    update

--- src/components-front-end/(BlogsManagement)/blogDetails.jsx
+++ src/components-front-end/(BlogsManagement)/blogDetails.jsx
@@ -46,6 +46,7 @@ export function BlogDetails({blog}){
                             {/* Banner */}
                             <div className="group overflow-hidden rounded-3xl border border-gray-100 bg-white shadow-lg">
                                 <img
+                                
                                     src={`${process.env.NEXT_PUBLIC_NODEJS_URL}/v1/uploads?filepath=blogs/${blog.image_url}`}
                                     alt="Medical surgery safety guide banner"
                                     className="
